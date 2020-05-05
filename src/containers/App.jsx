78aef29d7492d95b6b2d544ea674158c2e704026
Alessandri Fonseca <aafonsecab@hotmail.com>
2020-05-05 16:59:10 -0400

    main section complete

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,27 +1,56 @@
-import React from 'react'
-import Header from '../components/Header'
-import Search from '../components/Search'
-import Categories from '../components/Categories'
-import Caroussel from '../components/Caroussel'
-import CarousselItem from '../components/CarousselItem'
-import Footer from '../components/Footer'
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Search from "../components/Search";
+import Categories from "../components/Categories";
+import Caroussel from "../components/Caroussel";
+import CarousselItem from "../components/CarousselItem";
+import Footer from "../components/Footer";
 
-import '../assets/styles/app.scss'
+import "../assets/styles/app.scss";
 
-const App = () =>(
+const App = () => {
+  const [videos, setVideos] = useState({
+    'mylist': [],
+    'trends': [],
+    'originals': [],
+  });
+  useEffect(() => {
+    fetch("http://localhost:3000/initialState")
+      .then((response) => response.json())
+      .then((data) => setVideos(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  console.log(videos);
+
+  return (
     <div className="App">
-        <Header/>
-        <Search/>
-        <Categories>
-            <Caroussel>
-                <CarousselItem/>
-                <CarousselItem/>
-                <CarousselItem/>
-                <CarousselItem/>
-            </Caroussel>
+      <Header />
+      <Search />
+      {videos.mylist.length > 0 && 
+        <Categories className="mylist">
+          <Caroussel>
+            <CarousselItem />
+          </Caroussel>
         </Categories>
-        <Footer/>
+      }
+      <Categories className='trends'>
+          <Caroussel>
+            {videos.trends.map(item =>(
+              <CarousselItem key={item.id} {...item}/>
+            ))}
+          </Caroussel>
+      </Categories>
+      <Categories>
+        <Caroussel>
+          {videos.originals.map(item =>(
+            <CarousselItem key={item.id} {...item}/>
+          ))}
+        </Caroussel>
+      </Categories>
+     <Footer />
     </div>
-)
+  );
+};
 
 export default App;
