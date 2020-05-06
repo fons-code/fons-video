5a9ba4d93042e5d9c77fad81fd7b4938f0891d18
Alessandri Fonseca <aafonsecab@hotmail.com>
2020-05-05 20:59:42 -0400

    routes using react router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import '../assets/styles/components/header.scss'
 import logo from '../assets/static/logo-platzi-video-BW2.png'
 import user_icon from '../assets/static/user-icon.png'
 
 const Header = () => (
     <header className="header">
-    <img className="header__img" src={logo} alt="Platzi Video"/>
+    <Link to='/'>
+      <img className="header__img" src={logo} alt="Platzi Video"/>
+    </Link>
     <div className="header__menu">
       <div className="header__menu--profile">
         <img src={user_icon} alt=""/>
         <p>Perfil</p>
       </div>
       <ul>
-        <li><a href="/">Cuenta</a></li>
-        <li><a href="/">Cerrar Sesión</a></li>
+        <li>
+          <Link to='/login'>Cuenta</Link>
+        </li>
+        <li>
+        <Link to='/login'>iniciar sesion</Link>
+        </li>
       </ul>
     </div>
   </header>
