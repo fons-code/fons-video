6455c7de1d64e9514f9c5c3a42f9870efe0fa5de
Alessandri Fonseca <aafonsecab@hotmail.com>
2020-05-26 16:05:15 -0400

    add to favorite

--- src/components/CarousselItem.jsx
+++ src/components/CarousselItem.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
-import play_icon from '../assets/static/play-icon.png'
-import plus_icon from '../assets/static/plus-icon.png'
-import '../assets/styles/components/carousselItem.scss'
+import React from "react";
+import { connect } from "react-redux";
+import {setFavorite} from "../actions";
+import play_icon from "../assets/static/play-icon.png";
+import plus_icon from "../assets/static/plus-icon.png";
+import "../assets/styles/components/carousselItem.scss";
 
-const CarousselItem = ({cover, title, year, contentRating, duration}) =>(
+const CarousselItem = (props) => {
+  const { cover, title, year, duration, contentRating } = props;
+	const handleSetFavorite = () => {
+		props.setFavorite({
+			cover,
+			title,
+			year,
+			duration,
+			contentRating,
+		});
+	};
+  return (
     <div className="carousel-item">
-    <img className="carousel-item__img" src={cover} alt={title}  />
-    <div className="carousel-item__details">
-      <div>
-        <img className="carousel-item__details--img" src={play_icon} alt="Play Icon"/> 
-        <img className="carousel-item__details--img" src={plus_icon} alt="Plus Icon"/> 
+      <img className="carousel-item__img" src={cover} alt={title} />
+      <div className="carousel-item__details">
+        <div>
+          <img
+            className="carousel-item__details--img"
+            src={play_icon}
+            alt="Play Icon"
+          />
+          <img
+            className="carousel-item__details--img"
+            src={plus_icon}
+            alt="Plus Icon"
+            onClick={handleSetFavorite}
+          />
+        </div>
+        <p className="carousel-item__details--title">{title}</p>
+        <p className="carousel-item__details--subtitle">
+          {`${year} ${contentRating} ${duration}`}
+        </p>
       </div>
-<p className="carousel-item__details--title">{title}</p>
-      <p className="carousel-item__details--subtitle">
-        {`${year} ${contentRating} ${duration}`}
-      </p>
     </div>
-    </div>
-)
+  );
+};
+// Redux
+const mapDispatchToProps = {
+	setFavorite,
+};
 
-export default CarousselItem;
+// export default CarouselItem;
+export default connect(null, mapDispatchToProps)(CarousselItem);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,32 @@
 const reducer = (state, action) => {
-    return state
-}
+	switch (action.type) {
+		case 'SET_FAVORITE':
+			return {
+				...state,
+				mylist: [...state.mylist, action.payload],
+			};
+		default:
+			return state;
+	}
+};
 
 export default reducer;
+
+
+function findNb(m) {
+    let acum = 0
+    let count = 0
+    while(acum==m){
+      acum += (count + 1)**3
+      count ++
+	}
+	console.log(count -1)
+	console.log(acum)
+    if(acum == m) {
+    return count 
+    }else{
+      return (-1);
+    }
+}
+
+console.log(findNb(135440716410000));
